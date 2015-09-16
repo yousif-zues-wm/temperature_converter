9e758f9947e9ea81541789d3fa5b3fd67e034852
Zeus Yousif <zues-yousif@west-mec.org>
2015-09-16 16:59:57 -0700

    Finished

--- temp_converter.js
+++ temp_converter.js
@@ -3,8 +3,8 @@ var fahrenheit = prompt("Enter a number for fahrenheit");
 
 
 // Enter conversion code below...
-var fahrenheitToCelsius = ( fahernheit - 32) / 1.8;
-var fahrenheitToKelvin = ( fahernheit + 459.67) / 1.8;
+var fahrenheitToCelsius = ( fahrenheit - 32) / 1.8;
+var fahrenheitToKelvin = ( fahrenheit + 459.67) / 1.8;
 
 // Fahrenheit
 console.log(fahrenheit);
@@ -15,9 +15,15 @@ var celsius = prompt("Enter a number for celsius");
 var celsiustofahrenheit = celsius*1.8 + 32
 var celsiustokelvin = celsius+ 273.15
 //  Console.log For the two
-console.log("C =" + CelsiusToKelvin);
-console.log("C =" + CelsiusToFahrenheit);
+console.log("C =" + celsiustokelvin);
+console.log("C =" + celsiustofahrenheit);
 
 //Kelvin
 
 var kelvin =273.15;
+var kelvintocelsius = (kelvin - 273.15);
+var kelvintofahrenheit = (kelvin * 1.8 - 459.67);
+
+//console.logs
+console.log("K =" + kelvintocelsius);
+console.log("K =" + kelvintofahrenheit);
