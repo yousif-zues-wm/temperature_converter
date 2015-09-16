20df688cf20fa68040c7fd4ddb95fe462cb5e407
Zeus Yousif <zues-yousif@west-mec.org>
2015-09-15 18:11:08 -0700

    Almost finished, all done but kelvin

--- temp_converter.js
+++ temp_converter.js
@@ -1,12 +1,23 @@
 // Set this variable to a starting Fahrenheit temperature...
-var fahrenheit = 32;
+var fahrenheit = prompt("Enter a number for fahrenheit");
+
 
 // Enter conversion code below...
-var fahrenheitToCelsius = ...;
-var fahrenheitToKelvin = ...;
+var fahrenheitToCelsius = ( fahernheit - 32) / 1.8;
+var fahrenheitToKelvin = ( fahernheit + 459.67) / 1.8;
 
-// Enter console.log statements below...
+// Fahrenheit
+console.log(fahrenheit);
+console.log("C =" + fahrenheitToCelsius);
 
-// Repeat the above for Celsius and Kelvin...
-var celsius = 0;
-var kelvin = 273.15;
+// Celsius and Kelvin
+var celsius = prompt("Enter a number for celsius");
+var celsiustofahrenheit = celsius*1.8 + 32
+var celsiustokelvin = celsius+ 273.15
+//  Console.log For the two
+console.log("C =" + CelsiusToKelvin);
+console.log("C =" + CelsiusToFahrenheit);
+
+//Kelvin
+
+var kelvin =273.15;
