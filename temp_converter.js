1a38afb8365926e86c3349b6702f093a9f321e96
Zeus Yousif <zues-yousif@west-mec.org>
2015-09-17 16:31:50 -0700

    Finished the bonus and added the option to choose which temperature that you are converting

--- temp_converter.js
+++ temp_converter.js
@@ -1,29 +1,37 @@
+//Conditionals
+var temp =  prompt ("Please select a type of temperature to convert. (F, C, or K");
+
+
+
+if (temp === "F") {
 // Set this variable to a starting Fahrenheit temperature...
-var fahrenheit = prompt("Enter a number for fahrenheit");
+    var fahrenheit = prompt("Enter a number for fahrenheit");
 
 
 // Enter conversion code below...
-var fahrenheitToCelsius = ( fahrenheit - 32) / 1.8;
-var fahrenheitToKelvin = ( fahrenheit + 459.67) / 1.8;
+    var fahrenheitToCelsius = ( fahrenheit - 32) / 1.8;
+    var fahrenheitToKelvin = ( fahrenheit + 459.67) / 1.8;
 
 // Fahrenheit
-console.log("K =" + fahrenheitToKelvin);
-console.log("C =" + fahrenheitToCelsius);
-
+    console.log("K =" + fahrenheitToKelvin);
+    console.log("C =" + fahrenheitToCelsius);
+}
+else if (temp === "C") {
 // Celsius and Kelvin
-var celsius = prompt("Enter a number for celsius");
-var celsiustofahrenheit = celsius*1.8 + 32;
-var celsiustokelvin = celsius+ 273.15;
+    var celsius = prompt("Enter a number for celsius");
+    var celsiustofahrenheit = celsius * 1.8 + 32;
+    var celsiustokelvin = celsius + 273.15;
 //  Console.log For the two
-console.log("K =" + celsiustokelvin);
-console.log("F =" + celsiustofahrenheit);
-
+    console.log("K =" + celsiustokelvin);
+    console.log("F =" + celsiustofahrenheit);
+}
 //Kelvin
-
-var kelvin =prompt("Enter a number for kelvin");
-var kelvintocelsius = (kelvin - 273.15);
-var kelvintofahrenheit = (kelvin * 1.8 - 459.67);
+else if (temp === "K") {
+    var kelvin = prompt("Enter a number for kelvin");
+    var kelvintocelsius = (kelvin - 273.15);
+    var kelvintofahrenheit = (kelvin * 1.8 - 459.67);
 
 //console.logs
-console.log("C =" + kelvintocelsius);
-console.log("F =" + kelvintofahrenheit);
+    console.log("C =" + kelvintocelsius);
+    console.log("F =" + kelvintofahrenheit);
+}
